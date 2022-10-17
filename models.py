92ffcb17d6e713affda42aec0212e752e5ef9d8c
alira1984 <anderlira@yahoo.com.br>
2022-10-16 21:43:55 -0300

    Primeiro commit portfólio

---
+++ models.py
@@ -0,0 +1,22 @@
+#from main import db
+#from datetime import datetime
+
+
+
+
+
+
+#from main import database
+#from datetime import datetime
+
+#class Projetos(database.Model):
+    #id = database.Column(database.Integer, primary_key=True)
+    #projeto = database.Column(database.String, nullable=False)
+    #data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    #nome = database.Column(database.String(50))
+
+    #ef__init__(self, nome):
+        #self.nome = nome
+
+
+
